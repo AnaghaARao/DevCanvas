29932a80eeabeecdb861d133d3a5562c02a8d822
AnaghaARao <rao.anagha2002@gmail.com>
2024-09-25 12:25:02 +0530

    uploadMate models configured

---
+++ backend/uploadMate/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+import os
+
+# To get the name of the file being uploaded, you can use the name attribute of the file field. 
+# When a file is uploaded through the FileField, Django stores the file with its original name in 
+# the name attribute of the file object.
+
+def upload_to_author(instance, filename):
+    return os.path.join('uploads',instance.author,filename)
+
+# Create your models here.
+class FileNest(models.Model):
+    language = models.CharField(max_length=100) # to store programming language
+    author = models.CharField(max_length=100) # to store the username
+    docType = models.CharField(max_length=100) # to store type of documentation to be generated
+    dateTime = models.DateTimeField(default=timezone.now) # to store the time of upload
+    file = models.FileField(upload_to=upload_to_author)
