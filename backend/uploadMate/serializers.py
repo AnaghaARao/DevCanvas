573c9aeb2358fcd5cdb42a71ac5234761a0c71e5
AnaghaARao <rao.anagha2002@gmail.com>
2024-09-29 13:32:10 +0530

    serializer for api based rendering

---
+++ backend/uploadMate/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import FileNest
+
+class DocumentUploadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FileNest
+        fields = ['language','docType', 'file', 'author']
