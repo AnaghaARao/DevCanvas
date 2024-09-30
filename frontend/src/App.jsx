a874c08283d27efccd344771b2dc6ce64c5a73c4
Anjali <anjalibhatkal03@gmail.com>
2024-09-30 22:17:04 +0530

    email and username validation handled

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,9 +31,9 @@ function App() {
               </ProtectedRoute>
             }
           />
-          <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/register" element={<RegisterAndLogout />} />
+          <Route path="/authentication/login" element={<Login />} />
+          <Route path="/authentication/register" element={<Register />} />
+          <Route path="/authentication/logout" element={<Logout />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Form from "../components/Form";
 
 function Register() {
-  return <div>Register</div>;
+  return <Form route="/authentication/register" method="register" />;
 }
 
 export default Register;
