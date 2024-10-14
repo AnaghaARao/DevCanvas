e5ccdf83a7b047808de4bcc95148d9cf645e55c4
AnaghaARao <rao.anagha2002@gmail.com>
2024-10-14 17:11:53 +0530

    updated utils for dynamic diagram generation

--- backend/classDiagram/utils.py
+++ backend/classDiagram/utils.py
@@ -1,25 +1,30 @@
 import os
-from .java_class_diagram_generator import JavaDiagramGenerator
+from .class_diagram_generator import JavaDiagramGenerator, JavaScriptDiagramGenerator
 
 def process_file(file_path, language, author, doc_id):
     if language == 'java':
-        java_processor = JavaDiagramGenerator(file_path, author, doc_id)
-        result = java_processor.analyze_java_file()
+        processor = JavaDiagramGenerator(file_path, author, doc_id)
+    elif language == 'javascript':
+        processor = JavaScriptDiagramGenerator(file_path, author, doc_id)
+    else:
+        return {'error':f'Unsupported language: {language}'}
+    
+    result = processor.analyze_file()
 
-        if 'error' in result[0]:
-            # Return error message to be passed to the frontend
-            return {
-                'error': result[0]['error'],
-                'details': result[0]['details']
-            }
-
-        # Proceed with diagram generation if no errors
-        classes, methods = result
-
-        # Call diagram generation methods as usual
-        class_diagram_path, flowchart_paths = java_processor.save_diagrams(classes, methods)
-        
+    if 'error' in result[0]:
+        # Return error message to be passed to the frontend
         return {
-            'class_diagram': class_diagram_path,
-            'flowcharts': flowchart_paths
+            'error': result[0]['error'],
+            'details': result[0]['details']
         }
+
+    # Proceed with diagram generation if no errors
+    classes, methods = result
+
+    # Call diagram generation methods as usual
+    class_diagram_path, flowchart_paths = processor.save_diagrams(classes, methods)
+    
+    return {
+        'class_diagram': class_diagram_path,
+        'flowcharts': flowchart_paths
+    }
