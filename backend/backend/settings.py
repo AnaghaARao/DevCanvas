1f24a832b30f98bce0f789db7d8b0f970fc26573
AnaghaARao <rao.anagha2002@gmail.com>
2024-10-19 14:45:32 +0530

    added uploadMate into backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'authentication',
-    # 'uploadMate',
+    'uploadMate',
     # 'summaryGen',
     # 'classDiagram',
     'corsheaders',
