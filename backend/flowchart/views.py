b9dee1638c84762667cb8e42a84c6d392f139895
AnaghaARao <rao.anagha2002@gmail.com>
2024-11-14 15:45:40 +0530

    testing

--- backend/flowchart/views.py
+++ backend/flowchart/views.py
@@ -47,7 +47,7 @@ def generate_flowchart_view(request, doc_id):
     if not isinstance(file_path, str):
         return Response({'error': 'flowchart path is not valid'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    file_url = f"{settings.MEDIA_URL}/{author}/{directory}/{file_name}"
+    file_url = f"uploads/{author}/{file_name}"
 
     return Response({
         'message':'flowchart generated successfully',
