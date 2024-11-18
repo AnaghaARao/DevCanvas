e10b2d243936acd1ec51159a901d392df51019cc
AnaghaARao <rao.anagha2002@gmail.com>
2024-11-18 09:40:17 +0530

    updated file storage path

--- backend/flowchart/models.py
+++ backend/flowchart/models.py
@@ -5,8 +5,8 @@ import os
 
 # Define a function to dynamically set the upload path based on the author
 def upload_to_author(instance, filename):
-    # Structure: uploads/<author>/flowcharts/<dir_name>/<filename>
-    return os.path.join(settings.MEDIA_ROOT, instance.flowchart_nest.author, 'flowcharts', instance.flowchart_nest.dir_name, filename)
+    # Structure: uploads/<author>/<dir_name>/<filename>
+    return os.path.join(settings.MEDIA_ROOT, instance.flowchart_nest.author, instance.flowchart_nest.dir_name, filename)
 
 class FlowchartNest(models.Model):
     language = models.CharField(max_length=100)
