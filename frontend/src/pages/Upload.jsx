3e2542cf80acddb72f5cfd4f0569f9c5d5591508
AnaghaARao <rao.anagha2002@gmail.com>
2024-11-19 10:26:52 +0530

    testing

--- frontend/src/pages/Upload.jsx
+++ frontend/src/pages/Upload.jsx
@@ -11,9 +11,11 @@ const Upload = () => {
   const [language, setLanguage] = useState("");
   const [docType, setDocType] = useState("");
   const [dirName, setDirName] = useState("");
+  const [folderName, setFolderName] = useState("No folder selected");
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   const handleFileChange = (e) => {
     setFiles((prevFiles) => [...prevFiles, ...Array.from(e.target.files)]);
@@ -25,6 +27,7 @@ const Upload = () => {
       const firstFilePath = selectedFiles[0].webkitRelativePath;
       const folderName = firstFilePath.split("/")[0];
       setDirName(folderName);
+      setFolderName(folderName); // Update folder name
     }
     setFiles((prevFiles) => [...prevFiles, ...selectedFiles]);
   };
@@ -59,11 +62,8 @@ const Upload = () => {
       formData.append("files", file);
     });
 
-    for (let pair of formData.entries()) {
-      console.log(pair[0] + ": ", pair[1]);
-    }
-
     try {
+      setLoading(true);
       const response = await fetch("http://127.0.0.1:8000/uploadmate/upload/", {
         method: "POST",
         body: formData,
@@ -79,6 +79,7 @@ const Upload = () => {
           });
         } else {
           showSuccess("File Upload successfully");
+          navigate("/documentation");
         }
       } else {
         switch (response.status) {
@@ -99,6 +100,8 @@ const Upload = () => {
     } catch (error) {
       console.error("Error during file upload:", error);
       showError("Error during file upload, please try again");
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -109,6 +112,17 @@ const Upload = () => {
       <div className="upload-section">
         <div className="left-upload">
           <div className="upload-files">
+            {/* <div className="input-sec">
+              <label htmlFor="fileInput">Upload File:</label>
+              <input
+                type="file"
+                id="fileInput"
+                onChange={handleFileChange}
+                className="input-div"
+                accept=".txt, .docx, .pdf, .cpp, .java, .py"
+                required
+              />
+            </div> */}
             <div className="input-sec">
               <label htmlFor="folderInput">Upload Folder:</label>
               <input
@@ -119,7 +133,15 @@ const Upload = () => {
                 accept=".txt, .docx, .pdf, .cpp, .java, .py"
                 webkitdirectory="true"
                 required
+                style={{ display: "none" }}
               />
+              <button
+                type="button"
+                onClick={() => document.getElementById("folderInput").click()}
+                className="btn"
+              >
+                {folderName}
+              </button>
             </div>
           </div>
           {files.length > 0 && (
@@ -177,8 +199,14 @@ const Upload = () => {
             </select>
           </div>
 
-          <button type="submit" className="btn">
-            Generate Now <BoltIcon />
+          <button type="submit" className="btn" disabled={loading}>
+            {loading ? (
+              <>
+                Please wait while its processing... <BoltIcon />
+              </>
+            ) : (
+              "Generate Now"
+            )}
           </button>
         </div>
       </div>
@@ -187,147 +215,3 @@ const Upload = () => {
 };
 
 export default Upload;
-
-// import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import "../styles/general.css";
-// import "../styles/upload.css";
-// import BoltIcon from "@mui/icons-material/Bolt";
-// import { useDispatch, useSelector } from "react-redux";
-// import { showAlert, showError, showSuccess } from "../hooks/toastUtils";
-
-// const Upload = () => {
-//   const [file, setFile] = useState(null);
-//   const [language, setLanguage] = useState("");
-//   const [docType, setDocType] = useState("");
-//   const user = useSelector((state) => state.user);
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-
-//   const handleFileChange = (e) => {
-//     setFile(e.target.files[0]);
-//   };
-
-//   const handleLanguageChange = (e) => {
-//     setLanguage(e.target.value);
-//   };
-
-//   const handleDocTypeChange = (e) => {
-//     setDocType(e.target.value);
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     if (!file || !language || !docType) {
-//       showAlert("Please fill all fields and upload a file");
-//       return;
-//     }
-
-//     const formData = new FormData();
-//     formData.append("file", file);
-//     formData.append("language", language);
-//     formData.append("docType", docType);
-//     formData.append("author", user);
-
-//     try {
-//       const response = await fetch("http://127.0.0.1:8000/uploadmate/upload/", {
-//         method: "POST",
-//         body: formData,
-//       });
-
-//       const data = await response.json();
-//       if (response.ok) {
-//         if (data.message) {
-//           showSuccess(data.message);
-//           const fileUrl = `${import.meta.env.VITE_API_URL}${data.file_url}`;
-//           console.log(fileUrl);
-
-//           navigate("/documentation", {
-//             state: { fileUrl },
-//           });
-//         } else {
-//           showSuccess("File Upload successfully");
-//         }
-//       } else {
-//         switch (response.status) {
-//           case 400:
-//             showError(`Error: ${data.error || "Bad Request"}`);
-//             break;
-//           case 404:
-//             showError(`Error: ${data.error || "File not found"}`);
-//             break;
-//           case 500:
-//             showError(`Error: ${data.error || "Internal server error"}`);
-//             break;
-//           default:
-//             showError(`Error: ${data.error || "Unexpected error"}`);
-//             break;
-//         }
-//       }
-//     } catch (error) {
-//       console.error("Error during file upload:", error);
-//       showError("Error during file upload, please try again");
-//     }
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit} className="upload-form">
-//       <h2>Upload Files</h2>
-//       <hr className="divider" />
-//       <div className="upload-section">
-//         <div className="upload-files">
-//           <label htmlFor="fileInput">Upload File:</label>
-//           <input
-//             type="file"
-//             id="fileInput"
-//             onChange={handleFileChange}
-//             className="input-div"
-//             accept=".txt, .docx, .pdf, .cpp, .java, .py"
-//             required
-//           />
-//         </div>
-//         <div className="upload-info">
-//           <div className="language">
-//             <label htmlFor="languageInput" className="label-head">
-//               Programming Language:
-//             </label>
-//             <input
-//               type="text"
-//               id="languageInput"
-//               value={language}
-//               onChange={handleLanguageChange}
-//               placeholder="Enter programming language"
-//               className="input-div"
-//               required
-//             />
-//           </div>
-
-//           <div className="documentation">
-//             <label htmlFor="docTypeSelect" className="label-head">
-//               Documentation Type:
-//             </label>
-//             <select
-//               id="docTypeSelect"
-//               value={docType}
-//               onChange={handleDocTypeChange}
-//               className="input-div"
-//               required
-//             >
-//               <option value="">Select documentation type</option>
-//               <option value="summary">Summary</option>
-//               <option value="class diagram">Class Diagram</option>
-//               <option value="sequence diagram">Sequence Diagram</option>
-//               <option value="flowchart">Flowchart</option>
-//             </select>
-//           </div>
-
-//           <button type="submit" className="btn">
-//             Generate Now <BoltIcon />
-//           </button>
-//         </div>
-//       </div>
-//     </form>
-//   );
-// };
-
-// export default Upload;
