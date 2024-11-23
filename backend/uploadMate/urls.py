cd4dd79dee8ff7f31e9bbecf2817ce224aa9a680
AnaghaARao <rao.anagha2002@gmail.com>
2024-11-23 21:53:13 +0530

    updated api endpoints

--- backend/uploadMate/urls.py
+++ backend/uploadMate/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload/',views.upload_codebase, name='upload'),
+    path('uplink/',views.upload_codebase, name='uplink'),
     path('history/', views.history, name='history'),
 ]
