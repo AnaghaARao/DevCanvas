4fcb7030390b7c6951b3748b51a0747fe0b30a13
AnaghaARao <rao.anagha2002@gmail.com>
2024-11-28 17:07:55 +0530

    test run

--- backend/uploadMate/models.py
+++ backend/uploadMate/models.py
@@ -13,14 +13,14 @@ class FileNest(models.Model):
     author = models.CharField(max_length=100)  # Username or author of the files
     docType = models.CharField(max_length=100)  # Documentation type (e.g., summary, class diagram)
     dateTime = models.DateTimeField(default=timezone.now)  # Time of upload
-    dir_name = models.CharField(max_length=100, default='default_directory')  # Directory name for structuring uploads
+    dir_name = models.CharField(max_length=5000, default='default_directory')  # Directory name for structuring uploads
 
     def __str__(self):
         return f"FileNest {self.id} - {self.docType} by {self.author}"
 
 class FileEntry(models.Model):
     file_nest = models.ForeignKey(FileNest, on_delete=models.CASCADE, related_name='files')  # Relationship with FileNest
-    file = models.FileField(upload_to=upload_to_author)  # File field for each file in the directory
+    file = models.FileField(upload_to=upload_to_author, max_length=5000)  # File field for each file in the directory
 
     def __str__(self):
         return f"FileEntry {self.id} for {self.file_nest}"
